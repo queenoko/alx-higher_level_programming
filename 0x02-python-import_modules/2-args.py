c88084a897604fe506bac2ac11354798b2e31b63
queenoko <quimaudongwo@gmail.com>
2023-06-08 16:22:32 -0700

    argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     elif oko == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(oko))
     for i in range(oko):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
