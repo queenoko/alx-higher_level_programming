4769d56b6853ed361dcc94fddbcb93b8c7dcdf5a
queenoko <quimaudongwo@gmail.com>
2023-06-10 12:41:56 -0700

    del string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_word = ''
-    for z in my_string:
-        if z != 'c' and z != 'C':
-            new_word += z
-        return(new_word)
+    """remove c and C from string"""
+    copy = [z for z in my_string if z != 'c' and z != 'C']
+    return ("".join(copy))
