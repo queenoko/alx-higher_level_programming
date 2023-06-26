d8edd1103f7e2b49758f1e2461655a1e1b3941ce
queenoko <quimaudongwo@gmail.com>
2023-06-26 07:39:20 -0700

    prints first integer

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -15,7 +15,7 @@ def safe_print_list_integers(my_list=[], x=0):
     retun = 0
     for z in range(0, x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{:d}".format(my_list[z]), end="")
             retun += 1
         except (ValueError, TypeError):
             continue
