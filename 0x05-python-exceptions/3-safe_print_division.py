29db0886b35c629911b97cbd4cdde2a715dddf06
queenoko <quimaudongwo@gmail.com>
2023-06-26 07:46:24 -0700

    divides two integer

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+
+    """Returns division of a,b."""
+    
+    try:
+        divi = a / b
+    except (TypeError, ZeroDivisionError):
+        divi = None
+    finally:
+        print("Inside result: {}".format(divi))
+    return (divi)
