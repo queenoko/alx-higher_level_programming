579fa3a872cb5915f6f563a0177d7beab114eec7
queenoko <quimaudongwo@gmail.com>
2023-06-26 08:46:25 -0700

    prints an integer with error message

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+import sys
+
+def safe_print_integer_err(value):
+    """prints integer with "{:d}".format(0
+
+    If ValueError message exist, a correspoding message is printed to standard error
+
+    Arg:
+    value (int): the integer to print
+
+    Return:
+    If TypeError or ValurError Exist, return false else true
+    """
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (False)
