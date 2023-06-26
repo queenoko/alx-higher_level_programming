7c07cb2f50d87892bfaadcb6ea4a14b091e39880
queenoko <quimaudongwo@gmail.com>
2023-06-26 13:13:27 -0700

    magic calculation

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    result_cal = 0
+    for z in range(1, 3):
+        try:
+            if z > a:
+                raise Exception('Too far')
+            else:
+                result_cal += a ** b / z
+        except Exception:
+            result_cal = b + a
+            break
+        return (result_cal)
