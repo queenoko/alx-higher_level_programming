83fdbb0d12bd424052831abc6d9bcf8927387fca
queenoko <quimaudongwo@gmail.com>
2023-06-26 07:13:31 -0700

    print x element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,5 +15,5 @@ def safe_print_list(my_list=[], x=0):
             retun += 1
         except IndexError:
             break
-        print("")
-        return (retun)
+    print("")
+    return (retun)
