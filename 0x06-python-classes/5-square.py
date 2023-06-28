eeefc594629d82308db4a809f41fd6e0c9bb34a6
queenoko <quimaudongwo@gmail.com>
2023-06-28 03:03:08 -0700

    5square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -50,5 +50,5 @@ class Square:
         if self.__size == 0:
             print()
 
-    for i in range(self.__size):
-        print('#' * self.__size)
+        for i in range(self.__size):
+            print('#' * self.__size)
