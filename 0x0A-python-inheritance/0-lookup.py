9440c1ed97166ee1ae5c17c131a2760250ed190f
queenoko <quimaudongwo@gmail.com>
2023-07-10 08:27:55 -0700

    attribute and object list

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Available attributes and methods of an object
+are returned
+"""
+
+
+def lookup(obj):
+    """Functions that checks for attributes and methods of an object"""
+    return dir(obj)
