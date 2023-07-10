bf56928da321fa304d1e07c88a10fc2b5b429576
queenoko <quimaudongwo@gmail.com>
2023-07-10 11:00:32 -0700

    base geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines parent class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Base geometry class"""
+
+    def area(self):
+        """not implemented method"""
+        raise Exception("area() is not implemented")
