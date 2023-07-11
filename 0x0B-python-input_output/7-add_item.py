572efbcc08a1cf4e554cc7403349895444ef275c
queenoko <quimaudongwo@gmail.com>
2023-07-11 14:26:07 -0700

    adds argument

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
-        item_json = load_from_json_file("add_item.json")
+        items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        item_json = []
-    item_json.extend(sys.argv[1:])
-    save_to_json_file(item_json, "add_item.json")
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
