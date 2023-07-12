0a6d8744867ca47dfd452dfe51cbf0e764551703
queenoko <quimaudongwo@gmail.com>
2023-07-11 21:10:27 -0700

    appends file

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""function that inserts a line of text to a file"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends text after each line containing a given string in file"""
+    text1 = ""
+    with open(filename) as fil:
+        for line in fil:
+            text1 += line
+            if search_string in line:
+                text1 += new_string
+    with open(filename, "w") as wri:
+        wri.write(text)
