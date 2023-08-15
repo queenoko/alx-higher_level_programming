b81401af5abf982488d656f0c5e6453220f4f084
queenoko <quimaudongwo@gmail.com>
2023-08-15 15:57:42 -0700

    high score db

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- This database tabble Lists all records in second_table with a score >= 10...
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
