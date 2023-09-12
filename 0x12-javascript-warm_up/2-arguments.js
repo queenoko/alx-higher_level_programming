b01dd936593e607a622d903de8156b1813b27200
queenoko <quimaudongwo@gmail.com>
2023-09-11 19:13:40 -0700

    argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const argcCount = process.argv.length;
 
-if (argcCount > 0) {
+if (argcCount > 2) {
   console.log('Argument found');
-} else if (argcCount > 2) {
-  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
