541728cd21860ddce83cd7809900a887926bdfcd
queenoko <quimaudongwo@gmail.com>
2023-09-12 18:12:56 -0700

    rectangle attribute

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * This Represents a rectangle module  class with
+ * width and height attributes
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
