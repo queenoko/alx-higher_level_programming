0a44be4da8ed87cbda7cc15e831589721b208c7b
queenoko <quimaudongwo@gmail.com>
2023-09-28 18:51:36 -0700

    post url

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script sends a POST request to passed URL using curl, and display the body of response...
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
