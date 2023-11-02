3658a183638ba603a10b4c855748f64195454cfe
queenoko <quimaudongwo@gmail.com>
2023-11-01 19:39:55 -0700

    add

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,12 @@
+// add remove
+// $(document).ready(function () {
+	$("#add_item").click(function () {
+		$("<li>").text("Item").appendTo("ul.my_list");
+	});
+	$("#remove_item").click(function () {
+		$("ul.my_list li:last-child").remove();
+	});
+	$("#clear_list").click(function () {
+		$("ul.my_list").empty();
+	});
+});
